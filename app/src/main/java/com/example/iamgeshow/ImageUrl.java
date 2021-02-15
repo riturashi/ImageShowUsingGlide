7f9fc2fc39a87aba6fe8d757a6b9c337041d7a14
RASHI RITU <rashiritu.achieve@gmail.com>
2021-02-15 16:42:55 +0530

    ImageUploadUsingGlide

---
+++ app/src/main/java/com/example/iamgeshow/ImageUrl.java
@@ -0,0 +1,14 @@
+package com.example.iamgeshow;
+
+public class ImageUrl {
+
+        String imageUrl;
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+}
